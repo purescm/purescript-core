f8f8854f83374e174d7d2ba6bf338f5222e821ea
Antti Holvikari <antti@anttih.com>
2023-09-20 15:29:02 +0300

    Remove extra JS ffi

--- test/Test/Utils.js
+++ test/Test/Utils.js
@@ -5,18 +5,3 @@ exports.throwErr = function(msg) {
     throw new Error(msg);
   };
 };
-
-export const pureE = function (a) {
-  return function () {
-    return a;
-  };
-};
-
-export const bindE = function (a) {
-  return function (f) {
-    return function () {
-      return f(a())();
-    };
-  };
-};
-
