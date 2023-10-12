383f0f9dd026b38a8fe4a8a8d8a4ecec543fbb2c
Nathan Waltz <waltznr@gmail.com>
2023-10-12 12:00:42 -0700

    Implement foreign definitions for purescm (#1)

---
+++ test/Main.purs
@@ -0,0 +1,16 @@
+module Test.Main where
+
+import Prelude
+
+import Data.Lazy (defer, force)
+import Effect (Effect)
+import Effect.Console (log)
+import Test.Assert (assert)
+
+main :: Effect Unit
+main = do
+  assert (force lazyString == laziness)
+  log $ force lazyString
+  where
+    lazyString = defer \_ -> laziness
+    laziness = "I'm so lazy!"
