7754bc9f89d2e8a94e828f88128f2beab46f7423
Antti Holvikari <antti@anttih.com>
2024-02-16 14:22:55 +0200

    Use nix for CI to get latest Chez

---
+++ flake.nix
@@ -0,0 +1,55 @@
+{
+  description = "PureScript core packages CI setup";
+
+  inputs = {
+    # we are stuck on 23.05 because clang is broken on macos-x86 until macos 15:
+    # https://github.com/NixOS/nixpkgs/issues/234710
+    nixpkgs.url = "github:nixos/nixpkgs/release-23.05";
+
+    flake-compat = {
+      url = "github:edolstra/flake-compat";
+      flake = false;
+    };
+
+    purescript-overlay.url = "github:thomashoneyman/purescript-overlay";
+    purescript-overlay.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs = { self, nixpkgs, ... }@inputs:
+    let
+      supportedSystems = [ "x86_64-linux" "x86_64-darwin" "aarch64-darwin" ];
+
+      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+
+      nixpkgsFor = forAllSystems (system: import nixpkgs {
+        inherit system;
+        overlays = builtins.attrValues self.overlays;
+      });
+    in
+    {
+      overlays = {
+        purescript = inputs.purescript-overlay.overlays.default;
+      };
+
+      devShells = forAllSystems (system:
+        let pkgs = nixpkgsFor.${system};
+            chez = pkgs.chez-racket.overrideAttrs (final: prev: {
+              postFixup = if pkgs.stdenv.isDarwin then ''
+                install_name_tool -add_rpath ${pkgs.pcre2.out}/lib $out/bin/scheme
+              ''
+              else ''
+                patchelf $out/bin/scheme --add-rpath ${pkgs.pcre2.out}/lib
+              '';
+            });
+        in {
+          default = pkgs.mkShell {
+            name = "purescm";
+            packages = with pkgs; [
+              chez
+            ];
+          };
+        });
+
+      formatter = forAllSystems (system: nixpkgsFor.${system}.nixpkgs-fmt);
+    };
+}
